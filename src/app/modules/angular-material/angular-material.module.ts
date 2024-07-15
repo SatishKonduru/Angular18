fafdfff35bcc760270ab7d6e692d00b3202b90d5
satish <satishkonduru0225@gmail.com>
2024-07-15 12:53:24 +0530

    installed Angular Material and created Header component

--- src/app/modules/angular-material/angular-material.module.ts
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 
 
+const materialComponents = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule
+]
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [materialComponents],
+  exports: [materialComponents]
 })
 export class AngularMaterialModule { }
