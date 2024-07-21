7f6a21ac994c8f38e0779deeacc2aeccca576c08
satish <satishkonduru0225@gmail.com>
2024-07-21 19:27:04 +0530

    installed TailwindCSS and started with Router

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AngularMaterialModule } from '../../modules/angular-material/angular-material.module';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header', 
   standalone: true,
-  imports: [CommonModule, AngularMaterialModule],
+  imports: [CommonModule, AngularMaterialModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  preserveWhitespaces: true,
 })
 export class HeaderComponent {
 
