3daa2c472b74dbdf1c4a25ea268c3e510d55acbd
satish <satishkonduru0225@gmail.com>
2024-07-21 20:35:45 +0530

    working on Routes

--- src/app/components/course-details/course-details.component.ts
+++ src/app/components/course-details/course-details.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './course-details.component.css'
 })
 export class CourseDetailsComponent {
+  courseList = [
+    {id: 1, name: 'Angular', tutor: 'Satish'},
+    {id: 2, name: 'Angular Material', tutor: 'Satish Konduru'},
+    {id: 3, name: 'Bootstrap', tutor: 'RSK'},
+    {id: 4, name: 'NodeJS', tutor: 'Renu'},
+  ]
 
+  courseKeys: string[];
+
+  ngOnInit() {
+    this.courseKeys = Object.keys(this.courseList[0]);
+    console.log("Object.keys(this.courseList[0]):::::", Object.keys(this.courseList[0]))
+  }
 }
--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './course.component.css'
 })
 export class CourseComponent {
-
+courseList = [
+  {id: 1, name: 'Angular', tutor: 'Satish'},
+  {id: 2, name: 'Angular Material', tutor: 'Satish Konduru'},
+  {id: 3, name: 'Bootstrap', tutor: 'RSK'},
+  {id: 4, name: 'NodeJS', tutor: 'Renu'},
+]
 }
