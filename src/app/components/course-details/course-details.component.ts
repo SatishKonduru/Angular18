d1e444a0ad0aca7f0ae4694529c2e52d8960ddf5
satish <satishkonduru0225@gmail.com>
2024-07-24 21:20:18 +0530

    services

--- src/app/components/course-details/course-details.component.ts
+++ src/app/components/course-details/course-details.component.ts
@@ -1,32 +1,38 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CourseService } from '../../services/course.service';
 
 @Component({
   selector: 'app-course-details',
   standalone: true,
   imports: [],
   templateUrl: './course-details.component.html',
-  styleUrl: './course-details.component.css'
+  styleUrl: './course-details.component.css',
+  providers: [CourseService]
 })
 export class CourseDetailsComponent {
-  courseList = [
-    {id: 1, name: 'Angular', tutor: 'Satish'},
-    {id: 2, name: 'Angular Material', tutor: 'Satish Konduru'},
-    {id: 3, name: 'Bootstrap', tutor: 'RSK'},
-    {id: 4, name: 'NodeJS', tutor: 'Renu'}
-  ]
+  // courseList = [
+  //   {id: 1, name: 'Angular', tutor: 'Satish'},
+  //   {id: 2, name: 'Angular Material', tutor: 'Satish Konduru'},
+  //   {id: 3, name: 'Bootstrap', tutor: 'RSK'},
+  //   {id: 4, name: 'NodeJS', tutor: 'Renu'}
+  // ]
+  courseList: any = []
   router = inject(Router)
   courseKeys: string[];
   activatedRoute = inject(ActivatedRoute)
   selected = 'selected'
   courseId : any
+  courseService = inject(CourseService)
   ngOnInit() {
+    this.getCourseNames()
     this.activatedRoute.paramMap.subscribe(params => {
       if(params){
         this.courseId = params.get('id')
       }
     })
-    this.courseKeys = Object.keys(this.courseList[0]);
+    console.log("this.courseList[0]: ",this.courseList[0])
+   // this.courseKeys = Object.keys(this.courseList[0]);
     // console.log("Object.keys(this.courseList[0]):::::", Object.keys(this.courseList[0]))
   }
   onSelect(course: any){
@@ -38,4 +44,16 @@ export class CourseDetailsComponent {
   getSelected(course: any){
     return course.id == this.courseId
   }
+  getCourseNames(){
+   this.courseService.courseNames().subscribe({
+      next: (res: any) => {
+        console.log("res: ", res)
+          this.courseList = res
+        this.courseKeys = Object.keys(this.courseList[0]);
+      },
+      error: (err: any) => {
+        console.log("Error while getting course names", err)
+      }
+    })
+    }
 }
--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CourseService } from '../../services/course.service';
 
 @Component({
   selector: 'app-course',
   standalone: true,
   imports: [],
   templateUrl: './course.component.html',
-  styleUrl: './course.component.css'
+  styleUrl: './course.component.css',
+  providers: [CourseService]
 })
-export class CourseComponent {
-courseList = [
-  {id: 1, name: 'Angular', tutor: 'Satish'},
-  {id: 2, name: 'Angular Material', tutor: 'Satish Konduru'},
-  {id: 3, name: 'Bootstrap', tutor: 'RSK'},
-  {id: 4, name: 'NodeJS', tutor: 'Renu'},
-]
+export class CourseComponent implements OnInit{
+// courseList = [
+//   {id: 1, name: 'Angular', tutor: 'Satish'},
+//   {id: 2, name: 'Angular Material', tutor: 'Satish Konduru'},
+//   {id: 3, name: 'Bootstrap', tutor: 'RSK'},
+//   {id: 4, name: 'NodeJS', tutor: 'Renu'},
+// ]
+courseList: any = []
+courseService = inject(CourseService)
+ngOnInit(): void {
+    this.getCourseNames()
+}
+
+getCourseNames(){
+this.courseService.courseNames().subscribe({
+  next: (res: any) => {
+    console.log("res: ", res)
+    this.courseList = res
+  },
+  error: (err: any) => {
+    console.log("Error while getting course names", err)
+  }
+})
+}
+
 }
