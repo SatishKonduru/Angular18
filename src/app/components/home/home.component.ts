3094fb53780882c8afd04b6557e21e16c56a0452
satish <satishkonduru0225@gmail.com>
2024-07-24 17:32:25 +0530

    ViewEncapsulation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 import { AngularMaterialModule } from '../../modules/angular-material/angular-material.module';
@@ -9,13 +9,16 @@ import { CareersComponent } from "../careers/careers.component";
 import { ContactusComponent } from "../contactus/contactus.component";
 import { DemoComponent } from '../demo/demo.component';
 import { CommonModule } from '@angular/common';
+import { Comp1Component } from "../comp1/comp1.component";
+import { Comp2Component } from "../comp2/comp2.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AngularMaterialModule, AboutusComponent, ServicesComponent, ProductsComponent, CareersComponent, ContactusComponent, DemoComponent, CommonModule],
+  imports: [AngularMaterialModule, AboutusComponent, ServicesComponent, ProductsComponent, CareersComponent, ContactusComponent, DemoComponent, CommonModule, Comp1Component, Comp2Component],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+//  encapsulation: ViewEncapsulation.Emulated
 })
 export class HomeComponent {
   inputValue = ['Konduru']
