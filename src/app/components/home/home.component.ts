8b7c6d74bbab20bb1cb3ed60b601a61aabe238e3
satish <satishkonduru0225@gmail.com>
2024-08-01 08:28:03 +0530

    Working on Readonly signals

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, ViewEncapsulation } from '@angular/core';
+import { Component, computed, signal, ViewEncapsulation } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 import { AngularMaterialModule } from '../../modules/angular-material/angular-material.module';
@@ -13,6 +13,7 @@ import { Comp1Component } from "../comp1/comp1.component";
 import { Comp2Component } from "../comp2/comp2.component";
 import { PercentagePipe } from '../../pipes/percentage.pipe';
 import { SignalsComponent } from "../signals/signals.component";
+import { lastValueFrom } from 'rxjs';
 
 interface myCounter {
   value: number
@@ -29,8 +30,18 @@ interface myCounter {
 export class HomeComponent {
   inputValue = ['Konduru']
   check = 0
-  counter = signal<myCounter>({
-    value: 100
+  // counter = signal<myCounter>({
+  //   value: 100
+  // })
+  // counter = signal<number[]>([10])
+  counter = signal(0)
+  tenXCounter = computed(() => {
+    const value = this.counter()
+    return value * 10
+  })
+  hundredXCounter = computed(() => {
+    const value = this.tenXCounter()
+    return value * 10
   })
   ngOnInit(){
     setInterval(()=>{this.check++},1000)
@@ -55,10 +66,16 @@ export class HomeComponent {
     // this.counter.set({
     //   value: this.counter().value + 1
     // })
-    this.counter.update(counter => ({
-      ...counter,
-      value: counter.value + 1
-    }))
+    // this.counter.update(counter => ({
+    //   ...counter,
+    //   value: counter.value + 1
+    // }))
+    // this.counter.update(c => ([
+    //   ...c,
+    //   c[c.length - 1]+ 10
+    // ]))
+    this.counter.update(counter => counter + 1)
+    
   }
   decrement(){
     // this.counter--
@@ -69,9 +86,28 @@ export class HomeComponent {
     // this.counter.set({
     //   value: this.counter().value - 1
     // })
-    this.counter.update(c => ({
-      ...c,
-      value: c.value - 1
-    }))
+    // this.counter.update(c => ({
+    //   ...c,
+    //   value: c.value - 1
+    // }))
+    // this.counter.update(v => [
+    //   ...v,
+    //   v[v.length -1] - 10
+    // ])
+  }
+  append(){
+    // this.counter.update(v => [
+    //   ...v,
+    //   v[v.length - 1]+10
+    // ])
+    // const value = this.counter()
+    // const last = value[value.length - 1]
+    // console.log("this.counter().length: ", this.counter().length)
+    // console.log("this.counter[this.counter().length - 1]: ", this.counter().at(this.counter().length-1))
+    // const final = this.counter[this.counter().length - 1]
+    // this.counter().push(this.counter().at(this.counter().length-1) + 1)
+  }
+  remove(){
+    // this.counter().pop()
   }
 }
